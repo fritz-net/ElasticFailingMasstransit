7e180db8d7d9f23aee3cfba0d22d7b2590f1d650
user <git@andesia.org>
2023-11-04 17:44:35 +0100

    external apm project to fix issue there

--- DummyConsumer.cs
+++ DummyConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using Elastic.Apm;
 using Elastic.Apm.Api;
 using MassTransit;
+using System.Diagnostics;
 
 namespace PostsWorkerService.Notifications;
 
@@ -16,6 +17,17 @@ public class DummyConsumer : IConsumer<DummyEvent>
     // to show properly in kibana we need to create spans, else it is only shown in background service worker not in API service
     internal static ITransaction CreateTransaction<T>(ConsumeContext<T> context, string name, string type = "unknown") where T : class
     {
+        var redeliveryCount = context.GetHeader(MessageHeaders.RedeliveryCount);
+        if (redeliveryCount != null && redeliveryCount != "0") // workaround to fix issue with masstransit redelivery
+        {
+            return null;
+        }
+        if (Activity.Current != null /* || Activity.Current?.Baggage?.Any()*/)
+        {
+            //return null;
+            var baggage = Activity.Current?.Baggage.ToList();
+        }
+
         var distributedContext = context.GetHeader("x-distributed-tracing-context");
 
         var distributedTracingData = DistributedTracingData.TryDeserializeFromString(distributedContext);
@@ -30,7 +42,8 @@ public class DummyConsumer : IConsumer<DummyEvent>
 
     public async Task Consume(ConsumeContext<DummyEvent> context)
     {
-        var transaction = Agent.Tracer.CurrentTransaction ?? CreateTransaction(context, "ConsumeDummyEvent");
+        //var transaction = Agent.Tracer.CurrentTransaction ?? CreateTransaction(context, "ConsumeDummyEvent");
+        var transaction = CreateTransaction(context, "ConsumeDummyEvent");
         var span = transaction?.StartSpan("ConsumeDummyEvent", ApiConstants.TypeMessaging);
 
         _logger.LogInformation("DummyConsumer.Consume");
